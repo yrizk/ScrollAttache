be2300cdbe5b8d8c35ccf8b07e02347ac0da883b
yrizk <rizk.yohanna@gmail.com>
2015-05-02 13:08:23 -0400

    the discombobulation of the subviews is correctly mitigated by anchor view

--- app/src/main/java/com/yohan/scrollattache/Main.java
+++ app/src/main/java/com/yohan/scrollattache/Main.java
@@ -1,29 +1,23 @@
 package com.yohan.scrollattache;
 
-import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.util.AttributeSet;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 
 public class Main extends ActionBarActivity  implements CustomScrollView.ScrollListener{
 
-    public Button stickyButton;  // a sibling of the scrollview, becomes visible the moment dummyButton is completely off the screen
-    public Button dummyButton; // lives inside the scrollview
-    public TextView mainContent;
+    public Button stickyButton;  // a sibling of the scrollview, becomes visible the moment dummyStickyView is completely off the screen
+    public Button dummyStickyView; // lives inside the scrollview
+    private TextView ratingsView; //
     public CustomScrollView scrollView;
     private LinearLayout rootView;
     @Override
@@ -34,6 +28,8 @@ public class Main extends ActionBarActivity  implements CustomScrollView.ScrollL
 //        scrollView.setListener(this);
         stickyButton = (Button) findViewById(R.id.sticky_view);
         rootView = (LinearLayout) findViewById(R.id.outmost_parent);
+        dummyStickyView = (Button) findViewById(R.id.dummy_sticky_view);
+        ratingsView = (TextView) findViewById(R.id.ratings);
     }
 
 
@@ -61,24 +57,27 @@ public class Main extends ActionBarActivity  implements CustomScrollView.ScrollL
     }
 
     private void throwStickyBomb() {
-        ((ViewGroup)stickyButton.getParent()).removeView(stickyButton);
+        RelativeLayout.LayoutParams lp =(RelativeLayout.LayoutParams) ratingsView.getLayoutParams();
+        lp.addRule(RelativeLayout.BELOW,dummyStickyView.getId());
+        ratingsView.setLayoutParams(lp);
+        ((ViewGroup) stickyButton.getParent()).removeView(stickyButton);
+        dummyStickyView.setVisibility(View.INVISIBLE);
         rootView.addView(stickyButton,0);
-
     }
 
 
     @Override
     public void onScroll(int oldTop, int newTop) {
 //        Log.d("onScroll", "Old top : " + oldTop + " newTop : " + newTop);
-        if (newTop < (mainContent.getTop())) { //
-            Log.d("onScroll", "dummy button is visible");
-            stickyButton.setVisibility(View.GONE);
-            dummyButton.setVisibility(View.VISIBLE);
-    }
-        else if (newTop > (mainContent.getTop())) {
-            Log.d("onScroll", "dummy button is not visible");
-            stickyButton.setVisibility(View.VISIBLE);
-            dummyButton.setVisibility(View.GONE);
-        }
+//        if (newTop < (mainContent.getTop())) { //
+//            Log.d("onScroll", "dummy button is visible");
+//            stickyButton.setVisibility(View.GONE);
+//            dummyStickyView.setVisibility(View.VISIBLE);
+//    }
+//        else if (newTop > (mainContent.getTop())) {
+//            Log.d("onScroll", "dummy button is not visible");
+//            stickyButton.setVisibility(View.VISIBLE);
+//            dummyStickyView.setVisibility(View.GONE);
+//        }
     }
 }
