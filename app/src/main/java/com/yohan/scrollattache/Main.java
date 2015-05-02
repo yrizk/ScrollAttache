55033bcb04b0e4b63fe160f2b81a080eb664e3f4
yrizk <rizk.yohanna@gmail.com>
2015-05-02 12:06:36 -0400

    what what. the view is removed correctly and added as a sibling of the scrollview. great

--- app/src/main/java/com/yohan/scrollattache/Main.java
+++ app/src/main/java/com/yohan/scrollattache/Main.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
@@ -24,6 +25,7 @@ public class Main extends ActionBarActivity  implements CustomScrollView.ScrollL
     public Button dummyButton; // lives inside the scrollview
     public TextView mainContent;
     public CustomScrollView scrollView;
+    private LinearLayout rootView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +33,7 @@ public class Main extends ActionBarActivity  implements CustomScrollView.ScrollL
         scrollView = (CustomScrollView) findViewById(R.id.scroll);
 //        scrollView.setListener(this);
         stickyButton = (Button) findViewById(R.id.sticky_view);
+        rootView = (LinearLayout) findViewById(R.id.outmost_parent);
     }
 
 
@@ -59,6 +62,8 @@ public class Main extends ActionBarActivity  implements CustomScrollView.ScrollL
 
     private void throwStickyBomb() {
         ((ViewGroup)stickyButton.getParent()).removeView(stickyButton);
+        rootView.addView(stickyButton,0);
+
     }
 
 
