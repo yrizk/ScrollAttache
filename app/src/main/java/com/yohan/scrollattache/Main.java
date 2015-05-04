d95a0c9e77794b11b1889bcea3a85e442b3fddac
yrizk <rizk.yohanna@gmail.com>
2015-05-04 00:06:12 -0400

    added a ton of comments. cleaned things up

--- app/src/main/java/com/yohan/scrollattache/Main.java
+++ app/src/main/java/com/yohan/scrollattache/Main.java
@@ -2,36 +2,39 @@ package com.yohan.scrollattache;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
-import android.widget.Toast;
 
 
-public class Main extends ActionBarActivity  implements CustomScrollView.ScrollListener{
+public class Main extends ActionBarActivity  implements ObservedScrollView.ScrollListener{
 
     public Button stickyButton;  // a sibling of the scrollview, becomes visible the moment dummyStickyView is completely off the screen
-    private TextView ratingsView; //
-    public CustomScrollView scrollView;
-    private TextView descriptionView;
+    private View bottomView; //
+    public ObservedScrollView scrollView;
+    private View topView;
     private LinearLayout rootView;
+
+    /**
+     * the flag indicating the current state of the stickyButton
+     * if there are more other writes to this variable this becomes less of a good idea.
+     * Instead, consider view logic to determine where stickyButton is.
+     *
+     */
     private boolean stuckToTop = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        scrollView = (CustomScrollView) findViewById(R.id.scroll);
+        scrollView = (ObservedScrollView) findViewById(R.id.scroll);
         scrollView.setListener(this);
         stickyButton = (Button) findViewById(R.id.sticky_view);
         rootView = (LinearLayout) findViewById(R.id.outmost_parent);
-        ratingsView = (TextView) findViewById(R.id.ratings);
-        descriptionView = (TextView) findViewById(R.id.description);
+        bottomView =  findViewById(R.id.bottom_view);
+        topView =  findViewById(R.id.main_content);
     }
 
 
@@ -42,55 +45,48 @@ public class Main extends ActionBarActivity  implements CustomScrollView.ScrollL
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            if (stuckToTop) undoStickyBomb();
-            else throwStickyBomb();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
+    /*
+     * no black magic here , just switching the immediate parent to make stickyView a sibling of the scrollView.
+     */
     private void throwStickyBomb() {
-        RelativeLayout.LayoutParams lp =(RelativeLayout.LayoutParams) ratingsView.getLayoutParams();
-        lp.addRule(RelativeLayout.BELOW,descriptionView.getId());
-        ratingsView.setLayoutParams(lp);
+        RelativeLayout.LayoutParams lp =(RelativeLayout.LayoutParams) bottomView.getLayoutParams();
+        lp.addRule(RelativeLayout.BELOW, topView.getId());
+        bottomView.setLayoutParams(lp);
         ((ViewGroup) stickyButton.getParent()).removeView(stickyButton);
         rootView.addView(stickyButton, 0);
         stuckToTop = true;
     }
 
+    /**
+     * the  opposite of throwStickyBomb(), with the regeneration of stickyView's RelativeLayout.Params,
+     * which it looses the moment it becomes a child of the top root,  a LinearLayout.
+     */
     private void undoStickyBomb() {
 
-        RelativeLayout.LayoutParams lp =(RelativeLayout.LayoutParams) ratingsView.getLayoutParams();
+        RelativeLayout.LayoutParams lp =(RelativeLayout.LayoutParams) bottomView.getLayoutParams();
         lp.addRule(RelativeLayout.BELOW,stickyButton.getId());
-        ratingsView.setLayoutParams(lp);
+        bottomView.setLayoutParams(lp);
         lp = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        lp.addRule(RelativeLayout.BELOW,descriptionView.getId());
+        lp.addRule(RelativeLayout.BELOW, topView.getId());
         rootView.removeView(stickyButton);
         stickyButton.setLayoutParams(lp);
-        ((ViewGroup)ratingsView.getParent()).addView(stickyButton);
+        ((ViewGroup) bottomView.getParent()).addView(stickyButton);
         stuckToTop = false;
     }
 
 
     @Override
     public void onScroll(int oldTop, int newTop) {
-        Log.d("onScroll", " newTop : " + newTop + " height: " + stickyButton.getHeight() + " getTop: " + stickyButton.getTop());
         if(!stuckToTop) {
             if (newTop + stickyButton.getHeight() >= stickyButton.getTop()) {
-                Toast.makeText(this, "NOW", Toast.LENGTH_SHORT).show();
+                /**
+                 * One thing to note about this math, we  trigger the switch when the space just above
+                 * stickyView equals its actual height, which seems (to me) as the best moment to make the
+                 * transition seamless.
+                 */
                 throwStickyBomb();
             }
-        } else if (newTop <= ratingsView.getTop()) {
+        } else if (newTop <= bottomView.getTop()) {
             undoStickyBomb();
         }
     }
